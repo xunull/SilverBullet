9b8f6a0783ef746bb51d212962da9418a43c033b
崔启坤 <todogo@vip.163.com>
2016-08-18 11:22:04 +0800

    add  some snippets ,but safari 不支持不定参数，chrome 和 firefox 支持

--- index.js
+++ index.js
@@ -25,21 +25,29 @@ bullet_div.addEventListener('animationend',function(event){
 
 var zimus=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-function genereateBullet() {
+/**
+ * 一个弹幕的相关属性 
+ */
+function BulletComment(word) {
+	// 弹幕的颜色
+	this.color=null;
+	this.word=word;
+	
+}
+
+// 弹幕文字的数组
+var bulletComments =[];
+
+function genereateBullet(bulletComment) {
 	var bullet = document.createElement('span');
 	bullet.className='bullet';
 	var classList=bullet.classList;
 	classList.add('bullet','bullet-default-animation');
-	var bulletText='';
-	for(var i=0;i<5;i++){
-		var randomNum =Math.floor(Math.random()*26);
-		bulletText+=zimus[randomNum];
-		bullet.id=bulletText;
-	}
+	
 	var randomTop=Math.ceil(Math.random()*15);
 	bullet.style.top=30*randomTop+'px';
 	
-	var bulletTextNode = document.createTextNode(bulletText);
+	var bulletTextNode = document.createTextNode(bulletComment.word);
 	bullet.appendChild(bulletTextNode);
 	var bullet_div = document.getElementById('test1');
 	bullet_div.appendChild(bullet);
@@ -48,4 +56,26 @@ function genereateBullet() {
 //函数带() 是不同的
 //setInterval(genereateBullet(),1000);
 
-setInterval(genereateBullet,400);
+setInterval(temp,400);
+
+function temp() {
+	var bulletText='';
+	for(var i=0;i<5;i++){
+		var randomNum =Math.floor(Math.random()*26);
+		bulletText+=zimus[randomNum];
+//		bullet.id=bulletText;
+	}
+	var bulletComment = new BulletComment(bulletText);
+	addBullet(bulletComment);
+}
+
+/**
+ * 调用该方法即可生成一个弹幕
+ */
+function addBullet(...bulletComments) {
+	bulletComments.push(bulletComments);
+	for(var i=0;i<bulletComments.length;i++) {
+		var bulletComment=bulletComments.shift();
+		genereateBullet(bulletComment);
+	}
+}
